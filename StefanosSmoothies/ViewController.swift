c5cc0c3aac09909c0139e8bf7109d0c951e12474
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2018-11-05 18:30:38 -0500

    Initial Commit

---
+++ StefanosSmoothies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StefanosSmoothies
+//
+//  Created by Jaimilyn Vanderheyde on 2018-11-05.
+//  Copyright © 2018 Stefano Iaconetti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
