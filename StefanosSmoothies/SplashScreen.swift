bb0fb37dff1f6a017d33a8f3a41d3499b7092f61
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2018-11-06 22:26:18 -0500

    #2 created logo and placed it for all app icons

---
+++ StefanosSmoothies/SplashScreen.swift
@@ -0,0 +1,9 @@
+//
+//  SplashScreen.swift
+//  StefanosSmoothies
+//
+//  Created by Jaimilyn Vanderheyde on 2018-11-06.
+//  Copyright © 2018 Stefano Iaconetti. All rights reserved.
+//
+
+import Foundation
