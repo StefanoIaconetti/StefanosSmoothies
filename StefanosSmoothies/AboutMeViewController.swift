0bd3f448c9f540a17a9c8339a75dcb02548ec115
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2018-11-24 19:04:51 -0500

    #20 created a new viewcontroller and added a button that will direct user to my portfolio on the next commit

---
+++ StefanosSmoothies/AboutMeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutMeViewController.swift
+//  StefanosSmoothies
+//
+//  Created by Jaimilyn Vanderheyde on 2018-11-24.
+//  Copyright © 2018 Stefano Iaconetti. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class AboutMeViewController: UIViewController{
+    
+    @IBOutlet var linkToWebsite: UIButton!
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+}
