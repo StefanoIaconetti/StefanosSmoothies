b2069a9e4b3f3be5e4a3c61613413701b8274fe5
Web <web@amso0055z-40.local>
2018-12-04 10:07:39 -0500

    #26 Created the ViewEffectAnimatorType so i can use the same handler across the application

---
+++ StefanosSmoothies/Protocols/ViewEffectAnimatorType.swift
@@ -0,0 +1,27 @@
+//
+//  ViewEffectAnimatorType.swift
+//  StefanosSmoothies
+//
+//  Created by Web on 2018-12-04.
+//  Copyright © 2018 Stefano Iaconetti. All rights reserved.
+//
+
+import UIKit
+
+typealias ViewEffectAnimatorComplete = (UIViewAnimatingPosition) -> Void
+
+//Allows us to use the same handler type across app
+protocol ViewEffectAnimatorType {
+    var animator: UIViewPropertyAnimator { get}
+    init(targetView: UIView, onComplete:@escaping ViewEffectAnimatorComplete)
+    init(targetView: UIView, onComplete: @escaping ViewEffectAnimatorComplete, duration: TimeInterval)
+    
+    func startAnimation()
+}
+
+//calls the startAnimation function
+extension ViewEffectAnimatorType {
+    func startAnimation(){
+        animator.startAnimation()
+    }
+}
